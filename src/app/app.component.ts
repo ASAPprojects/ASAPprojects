fde941a9c65901f9816a6fcd605c8f2ef2aa2e78
ASAPprojects <asap.projects17@gmail.com>
2017-02-27 21:16:19 +0530

    photo slide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
- 
+	public current:number = 1;
+    public pics:Array<string> = [];
+    constructor(){
+        this.init();
+    }
+    init(){
+        var nameOfPicFolder = 'src/assets/images';
+        var noOfPics = 3;
+        var counter = 1;
+        while (counter <= noOfPics) {
+            this.pics.push(nameOfPicFolder+'/image'+counter+'.jpg');
+            counter++;
+        }
+		this.current = 0;
+    }
+	
+	isCurrent(newValue:number){
+		this.current == newValue ? true : false;
+        return this.current;
+    }
+
+	setInterval(() => this.current = (this.current < 3) ? (this.current + 1) : 0, 5000);
 }
